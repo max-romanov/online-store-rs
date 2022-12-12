f06ceeba9011653507bc7907e35da8a866ac6ff4
max-romanov <max.romanov.90@mail.ru>
2022-12-12 19:25:51 +0200

    feat: add server

---
+++ server/fruits/data.js
@@ -0,0 +1,19 @@
+const data = [
+  {
+    name: "passion fruit",
+    images: [
+      "https://static.libertyprim.com/files/familles/grenadille-large.jpg?1569489207",
+      "https://www.alphamega.com.cy/Files/Images/Ecom/Products/901444.jpg"
+    ],
+    colors: ["purple", "yellow"]
+  },
+  {
+    name: "orange",
+    images: [
+      "https://producemadesimple.ca/wp-content/uploads/2015/01/orange-web-600x450.jpg"
+    ],
+    colors: ["orange"]
+  }
+]
+
+export default data
